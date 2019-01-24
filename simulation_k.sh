7f0b30431301ac0549b838b3d593ce57a27afeeb
Nuttuso14 <nuttuso14@gmail.com>
2019-01-25 04:08:20 +0800

    shell_k

---
+++ simulation_k.sh
@@ -0,0 +1,12 @@
+./bestWifiAp 10000 5 1 3 3 3 3 3 1 2 3 4 5
+./bestWifiAp 10000 5 2 3 3 3 3 3 1 2 3 4 5
+./bestWifiAp 10000 5 3 3 3 3 3 3 1 2 3 4 5
+./bestWifiAp 10000 5 4 3 3 3 3 3 1 2 3 4 5
+./bestWifiAp 10000 5 5 3 3 3 3 3 1 2 3 4 5
+./bestWifiAp 10000 5 6 3 3 3 3 3 1 2 3 4 5
+./bestWifiAp 10000 5 7 3 3 3 3 3 1 2 3 4 5
+./bestWifiAp 10000 5 8 3 3 3 3 3 1 2 3 4 5
+./bestWifiAp 10000 5 9 3 3 3 3 3 1 2 3 4 5
+./bestWifiAp 10000 5 10 3 3 3 3 3 1 2 3 4 5
+./bestWifiAp 10000 5 11 3 3 3 3 3 1 2 3 4 5
+exec ./drawFigure
